bcef10425bf29f87662abcbdf1f562ac615f433c
tran.van.thangb <tran.van.thang-b@sun-asterisk.com>
2024-12-16 17:26:57 +0700

    Upgrade

--- android/src/main/kotlin/com/victorblaess/native_flutter_proxy/FlutterProxyPlugin.kt
+++ android/src/main/kotlin/com/victorblaess/native_flutter_proxy/FlutterProxyPlugin.kt
@@ -6,35 +6,20 @@ import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
-import io.flutter.plugin.common.PluginRegistry.Registrar
 import java.util.*
 
 /** FlutterProxyPlugin */
 public class FlutterProxyPlugin : FlutterPlugin, MethodCallHandler {
 
-    private var mMethodChannel: MethodChannel? = null;
-
-    companion object {
-        @JvmStatic
-        fun registerWith(registrar: Registrar) {
-            val instance = FlutterProxyPlugin()
-            instance.onAttachedToEngine(registrar.messenger());
-        }
-    }
-
-    private fun onAttachedToEngine(messenger: BinaryMessenger) {
-        mMethodChannel = MethodChannel(messenger, "native_flutter_proxy")
-        mMethodChannel!!.setMethodCallHandler(this)
-    }
+    private lateinit var mMethodChannel : MethodChannel
 
     override fun onAttachedToEngine(binding: FlutterPlugin.FlutterPluginBinding) {
         mMethodChannel = MethodChannel(binding.binaryMessenger, "native_flutter_proxy")
-        mMethodChannel!!.setMethodCallHandler(this)
+        mMethodChannel.setMethodCallHandler(this)
     }
 
     override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {
-        mMethodChannel!!.setMethodCallHandler(null)
-        mMethodChannel = null
+        mMethodChannel.setMethodCallHandler(null)
     }
 
     override fun onMethodCall(call: MethodCall, result: Result) {
